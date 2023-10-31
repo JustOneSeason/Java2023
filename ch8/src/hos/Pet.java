2006bc027aafb4edb162bb0cfd70f315e7dcc8ac
JustOneSeason <hc294240@gmail.com>
2023-10-31 10:39:27 +0900

    10.31
    10.31

---
+++ ch8/src/hos/Pet.java
@@ -0,0 +1,23 @@
+package hos;
+
+public interface Pet {
+	
+//	protected String owner;
+//
+//	
+//	public Pet(String owner) {
+//		
+//		this.owner = owner;
+//	}
+//	
+//	public  String OwnerName() {
+//		
+//		return owner;
+//	}
+//	
+//	public abstract void Cry();
+	
+	String OwnerName();
+	void Cry();
+
+}
