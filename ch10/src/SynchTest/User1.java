d67c98a0b7e2ed48de01618677729b6f481f77ac
JustOneSeason <hc294240@gmail.com>
2023-12-05 09:53:05 +0900

    12.05

---
+++ ch10/src/SynchTest/User1.java
@@ -0,0 +1,15 @@
+package SynchTest;
+
+public class User1 extends Thread{
+	
+	private Calculator cal;
+	
+	public void setCalculator(Calculator cal) {
+		this.cal = cal;
+		this.setName("User1");
+	}
+	
+	public void run() {
+		cal.setMemory(100);
+	}
+}
---
+++ ch10/src/SynchTest/User2.java
@@ -0,0 +1,16 @@
+package SynchTest;
+
+public class User2 extends Thread{
+	
+private Calculator cal;
+	
+	public void setCalculator(Calculator cal) {
+		this.cal = cal;
+		this.setName("User2");
+	}
+	
+	public void run() {
+		cal.setMemory(50);
+	}
+
+}
